646b668a1324ff0f8042a537c2e43c829201ae0e
Lilium765 <65602343+Lilium765@users.noreply.github.com>
2020-05-20 01:18:41 +0900

    Add files via upload

--- discordbot.py
+++ discordbot.py
@@ -6,14 +6,6 @@ import random
 bot = commands.Bot(command_prefix='*')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send('お兄ちゃん、それはわからないよ。')
-
-
 @bot.command()
 async def hello(ctx):
     await ctx.send('こんにちは、お兄ちゃん。')
@@ -30,5 +22,10 @@ async def roll(dice : str):
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await bot.say(result)
 
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send('お兄ちゃん、それはわからないよ。')
 
 bot.run(token)
