b5594faab25d1b7c639bffd97c1e56ed09dab129
Lilium765 <65602343+Lilium765@users.noreply.github.com>
2020-05-20 23:55:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,8 +2,6 @@ from discord.ext import commands
 import os
 import traceback
 import random
-import discord
-import nDnDICE
 
 bot = commands.Bot(command_prefix='*')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -11,13 +9,6 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.command()
 async def hello(ctx):
     await ctx.send('こんにちは、お兄ちゃん。')
-	
-@bot.event
-async def on_message(ctx):
-    msg = ctx.content
-    result = nDnDICE.nDn(msg)
-    if result is not None:
-        await client.send_message(ctx.channel, result)
 
 @bot.event
 async def on_command_error(ctx, error):
